0a94fafda1121cf3a0b825055e91523cfd61761c
sunnao <osa125125@naver.com>
2022-12-04 10:53:33 +0900

    [refactor] : params값 으로 데이터바인딩 방법 리팩토링 배열 index가 아닌 데이터id값으로 연결, Array.find() 메소드 사용, param 오류도 수정

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -2,9 +2,12 @@ import { useParams } from 'react-router-dom';
 
 const Detail = (props) => {
   let {id} = useParams();
+  let detailProduct = props.candles.find((data) => data.id === Number(id))
+  console.log(detailProduct)
 
   // params의id가 props.candles.id에 해당하는게 없으면 없는상품이라고 알려주기
-  if(!(props.candles.hasOwnProperty(id))){
+  if(!detailProduct){
+  // if(!(props.candles.hasOwnProperty(id))){
     return <div style={{ marginTop:'200px' }}>해당 상품이 존재하지 않습니다.</div>
   }
   return (
@@ -14,9 +17,9 @@ const Detail = (props) => {
         <img src={process.env.PUBLIC_URL +`/Candle${Number(id)+1}.jpg`} width="100%" />
       </div>
       <div className="col-md-6">
-        <h4 className="pt-5">{props.candles[id].title}</h4>
-        <p>{props.candles[id].content}</p>
-        <p>{props.candles[id].price.toLocaleString()}원</p>
+        <h4 className="pt-5">{detailProduct.title}</h4>
+        <p>{detailProduct.content}</p>
+        <p>{detailProduct.price.toLocaleString()}원</p>
         <button className="btn btn-danger">주문하기</button> 
       </div>
     </div>
