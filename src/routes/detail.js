22a44f53b700e8c17bafe5f2c19b3b63fc6f8a20
sunnao <osa125125@naver.com>
2022-12-06 22:10:06 +0900

    [feat] : styled-components사용 스타일컴포넌트 사용한 버튼으로 수정(주문하기)

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,9 +1,19 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+
+const OrderBtn = styled.button`
+box-shadow: 0px 10px 14px -7px grey;
+	background:black;
+  border : none;
+	border-radius:5px;
+	color:#ffffff;
+	padding: 7px 80px;
+`
 
 const Detail = (props) => {
   let {id} = useParams();
   let detailProduct = props.candles.find((data) => data.id === Number(id))
-  console.log(detailProduct)
 
   // params의id가 props.candles.id에 해당하는게 없으면 없는상품이라고 알려주기
   if(!detailProduct){
@@ -13,6 +23,7 @@ const Detail = (props) => {
   return (
   <div className="container">
     <div className="row">
+      
       <div className="col-md-6">
         <img src={process.env.PUBLIC_URL +`/Candle${Number(id)+1}.jpg`} width="100%" />
       </div>
@@ -20,7 +31,7 @@ const Detail = (props) => {
         <h4 className="pt-5">{detailProduct.title}</h4>
         <p>{detailProduct.content}</p>
         <p>{detailProduct.price.toLocaleString()}원</p>
-        <button className="btn btn-danger">주문하기</button> 
+        <OrderBtn>주문하기</OrderBtn>
       </div>
     </div>
   </div> 
