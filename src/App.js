83b84a4096ae97843c2ea3e009946a70fc450b29
sunnao <osa125125@naver.com>
2022-12-07 16:03:23 +0900

    [feat] : useParams-detail페이지, useEffect사용 이미지 클릭 시 이동, 할인창, 수량입력 경고창

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
-import { useState } from 'react';
-import candlesData from './data.js'
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
-import  Detail  from './routes/detail';
-import  About  from './routes/about';
-import Company from './routes/company';
-import Location from './routes/location';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import candlesData from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/detail";
+import About from "./routes/about";
+import Company from "./routes/company";
+import Location from "./routes/location";
 
 function App() {
   let [candles, setCandles] = useState(candlesData);
@@ -15,48 +15,76 @@ function App() {
 
   return (
     <div className="App">
-
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand style={{cursor: 'pointer'}} onClick={()=>{navigate('/')}}>Candle.SHOP</Navbar.Brand>
+          <Navbar.Brand
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Candle.SHOP
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link className='navLink' onClick={()=>{navigate('/')}}>Home</Nav.Link>
-            <Nav.Link className='navLink' onClick={()=>{navigate('/detail')}}>Products</Nav.Link>
-            <Nav.Link className='navLink' href="/about">Company</Nav.Link>
+            <Nav.Link
+              className="navLink"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              className="navLink"
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Products
+            </Nav.Link>
+            <Nav.Link className="navLink" href="/about">
+              Company
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <Routes>
-        <Route path="/" element={
-          <>
-          <div className="main-bg"></div>
-            <Row>
-              {candles.map((data, i) =>
-                <Product candles={candles[i]} num={i} key={i} />
-                )}
-            </Row>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <Row>
+                {candles.map((data, i) => (
+                  <Product candles={candles[i]} num={i} key={i} />
+                ))}
+              </Row>
+              <button onClick={() => {}}></button>
             </>
-        } />
+          }
+        />
         <Route path="/detail/:id" element={<Detail candles={candles} />} />
-        <Route path="/about" element={<About />}> 
-          <Route path="company" element={<Company />}/> {/* /about/company */}
-          <Route path="location" element={<Location />}/> {/* /about/location */}
+        <Route path="/about" element={<About />}>
+          <Route path="company" element={<Company />} /> {/* /about/company */}
+          <Route path="location" element={<Location />} /> {/* /about/location */}
         </Route>
-        <Route path="*" element={<div style={{ marginTop:'200px' }}>존재하지 않는 페이지 입니다.</div>} />
+        <Route path="*" element={<div style={{ marginTop: "200px" }}>존재하지 않는 페이지 입니다.</div>} />
       </Routes>
     </div>
-  )
+  );
 }
 
-const Product = (props) => {
-  return(
-    <Col sm className='products'>
-      <img src={process.env.PUBLIC_URL +`/Candle${props.num+1}.jpg`} width='100%' />
+const Product = props => {
+  return (
+    <Col sm className="products">
+      <Link to={/detail/ + `${props.num}`}>
+        <img src={process.env.PUBLIC_URL + `/Candle${props.num + 1}.jpg`} width="100%" />
+      </Link>
       <h4>{props.candles.title}</h4>
       <p>{props.candles.price.toLocaleString()}원</p>
     </Col>
-  )
-}
+  );
+};
 
 export default App;
--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
 
 
 const OrderBtn = styled.button`
@@ -12,8 +13,25 @@ box-shadow: 0px 10px 14px -7px grey;
 `
 
 const Detail = (props) => {
+  
   let {id} = useParams();
-  let detailProduct = props.candles.find((data) => data.id === Number(id))
+  let detailProduct = props.candles.find((data) => data.id === Number(id));
+  let [alertDiv, setAlertDiv]=useState(true);
+  let [num, setNum]=useState('')
+  
+  // 2초 뒤에 alert state를 false로 변경함.
+  // useEffect의 두번째 파라미터로 []넣으면 처음 mount될 때만 실행됨. 이후 재렌더링시 실행X
+  useEffect(()=>{
+    let a = setTimeout(()=>{setAlertDiv(false)},2000)
+    return ()=>{clearTimeout(a)} /* -> useEffect 동작 전에 실행 (cleanup function), unmount 될 때 실행 */
+  },[num])
+
+  useEffect(()=>{
+    if(isNaN(num)===true){
+      return alert('숫자만 입력 가능합니다.')
+    }
+  })
+
 
   // params의id가 props.candles.id에 해당하는게 없으면 없는상품이라고 알려주기
   if(!detailProduct){
@@ -22,8 +40,12 @@ const Detail = (props) => {
   }
   return (
   <div className="container">
+    {
+      alertDiv === true
+      ? <div className="alert alert-warning">2초 이내 구매 시 할인!!</div>
+      : null
+    }
     <div className="row">
-      
       <div className="col-md-6">
         <img src={process.env.PUBLIC_URL +`/Candle${Number(id)+1}.jpg`} width="100%" />
       </div>
@@ -31,6 +53,7 @@ const Detail = (props) => {
         <h4 className="pt-5">{detailProduct.title}</h4>
         <p>{detailProduct.content}</p>
         <p>{detailProduct.price.toLocaleString()}원</p>
+        <input placeholder="주문 수량" onChange={(e)=>{ setNum(e.target.value) }}></input>
         <OrderBtn>주문하기</OrderBtn>
       </div>
     </div>
@@ -39,3 +62,9 @@ const Detail = (props) => {
 }
 
 export default Detail;
+
+
+// useEffect(()=>{}) /**재랜더링마다 코드실행 */
+// useEffect(()=>{},[]) /** mount시 1회만 실행 */
+// useEffect(()=>{ return ()=>{} }) /** 실행전 cleanup function */
+// useEffect(()=>{ return ()=>{} },[]) /** unmount시 1회만 실행 */
